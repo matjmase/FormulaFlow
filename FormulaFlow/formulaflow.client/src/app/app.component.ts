302e645ac40582268fb70842b80c71caf703b2b0
Matt Masengill <67738268+matjmase@users.noreply.github.com>
2026-07-28 06:04:09 -0500

    Angular update 22

--- FormulaFlow/formulaflow.client/src/app/app.component.ts
+++ FormulaFlow/formulaflow.client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { HttpClient } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ChangeDetectionStrategy } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   standalone: false,
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {}
