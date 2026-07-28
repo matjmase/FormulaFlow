d49b326f5b89901cf925c12799959173d78891f1
Matt Masengill <67738268+matjmase@users.noreply.github.com>
2026-07-28 05:55:09 -0500

    Frontend SCSS migrate

--- FormulaFlow/formulaflow.client/src/app/app.component.ts
+++ FormulaFlow/formulaflow.client/src/app/app.component.ts
@@ -1,38 +1,10 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent implements OnInit {
-  public forecasts: WeatherForecast[] = [];
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.getForecasts();
-  }
-
-  getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-
-  title = 'formulaflow.client';
-}
+export class AppComponent {}
